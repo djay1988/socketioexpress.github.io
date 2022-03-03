1fd29d7bb3ec1e530add2844692f332a709cba99
Dilshan Jayasanka <dilshan.jayasanka88@gmail.com>
2022-03-03 09:29:32 +0530

    first Build

--- src/app/_services/pdf-gen.service.ts
+++ src/app/_services/pdf-gen.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import pdfMake from "pdfmake/build/pdfmake.min";
+
 
 @Injectable({
   providedIn: 'root'
